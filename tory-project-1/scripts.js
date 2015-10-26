ed6f6bca12e44beb4d52864eadf505ecadeb40df
Tory Burgett <vdburgett@gmail.com>
2015-10-26 11:54:21 -0400

    Answer Counter working

--- tory-project-1/scripts.js
+++ tory-project-1/scripts.js
@@ -17,59 +17,133 @@ $(document).ready(function(){
 
   // only one choice is selected
   $(".answerChoice").on("click", function(){
-    if ($(this).css("background")!=selectedColor){
-      $(".answerChoice").css("background", unselectedColor);
-      $(this).css("background", selectedColor);
+    if ($(this).css("background-color")!=selectedColor){
+      $(".answerChoice").css("background-color", unselectedColor);
+      $(this).css("background-color", selectedColor);
     }
   });
 
-//When on the right question, check answer pulls up the right answerType and verifies that the correct answer has the right background color when submit is clicked
-  var checkAnswer = function(questionNumber, answerType, correctAnswer){
-    if (questionCounter===questionNumber){
-      $(".answerArea").hide();
-      $(answerType).show();
-      $(".submitButton").show();
+  //When on the right question, check answer pulls up the right answerType and verifies that the correct answer has the right background-color color when submit is clicked
+var checkAnswer = function(questionNumber, answerType, correctAnswer){
 
-      $(".submitButton").on("click", function(){
-        event.preventDefault();
-        if ($(correctAnswer).css("background") == selectedColor){
-          answerCounter++;
-        }
-        else{
-          console.log("wrong answer");
-        }
-        // else if (($(correctAnswer).css("background")!=selectedColor)&&()) {
-        //
-        // }
+if(questionCounter===1){
+    //Answers - hide, then show relevant answer answer
+    // $(".answerArea").show();
+    $("#multipleChoice").hide();
+    $("#itsTrueFalse").show();
 
-        $(".nextArea").show();
-      });
-      // $(".submitArea").hide();
+    //Buttons - hide next show submit
+    $(".nextButton").hide();
+    $(".submitButton").show();
 
-    }
-  };
+    //set correct answer
+    correctAnswer = "#true";
+
+
+    //add event listener for submit
+    $(".submitButton").on("click", function(){
+      event.preventDefault();
+      //what is the correctAnswer's background-color color?
+      var correctAnswerColor;
+      correctAnswerColor = $(correctAnswer).css("background-color");
+
+      if(correctAnswerColor === selectedColor){
+        answerCounter++;
+        console.log("First if worked "+answerCounter);
+      }
+      else if(correctAnswerColor === unselectedColor){
+        console.log("Wrong "+ answerCounter);
+      }
+      //Buttons
+      $(".nextButton").show();
+      $(".submitButton").hide();
+    });
+  }
+};
+
+
+
+
+
+  // var checkAnswer = function(questionNumber, answerType, correctAnswer){
+  //   if (questionCounter===questionNumber){
+  //     $(".answerArea").hide();
+  //     $(answerType).show();
+  //     $(".submitButton").show();
+  //
+  //     $(".submitButton").on("click", function(){
+  //       event.preventDefault();
+  //       if ($(correctAnswer).css("background-color", selectedColor)){
+  //         answerCounter++;
+  //       }
+  //       else{
+  //         console.log("wrong answer");
+  //       }
+  //       // else if (($(correctAnswer).css("background-color")!=selectedColor)&&()) {
+  //       //
+  //       // }
+  //
+  //       $(".nextArea").show();
+  //     });
+  //     // $(".submitArea").hide();
+  //
+  //   }
+  // };
+
+var correctAnswer;
 
 
   $(".nextButton").on("click", function(){
     event.preventDefault();
     $(".nextButton").text("NEXT");
+    console.log("first ask"+questionCounter);
     if (questionCounter < (numOfQuestions+1)){
       console.log("questionCounter "+questionCounter);
 
-      $(".nextButton").hide();
       $("#question"+(questionCounter-1)).hide();
       $("#question"+questionCounter).show();
 
       //get into what answers to show, and how to grade the answers
       //temp guide: answer to first=true, second=B, third=false.
 
-    // if 
+      // if(questionCounter===1){
+      //   //Answers - hide, then show relevant answer answer
+      //   // $(".answerArea").show();
+      //   $("#multipleChoice").hide();
+      //   $("#itsTrueFalse").show();
+      //
+      //   //Buttons - hide next show submit
+      //   $(".nextButton").hide();
+      //   $(".submitButton").show();
+      //
+      //   //set correct answer
+      //   correctAnswer = "#true";
+      //
+      //   //add event listener for submit
+      //   $(".submitButton").on("click", function(){
+      //     event.preventDefault();
+      //     //what is the correctAnswer's background-color color?
+      //     var correctAnswerColor;
+      //     correctAnswerColor = $(correctAnswer).css("background-color");
+      //
+      //     if(correctAnswerColor === selectedColor){
+      //       answerCounter++;
+      //       console.log("First if worked "+answerCounter);
+      //     }
+      //     else if(correctAnswerColor === unselectedColor){
+      //       console.log("Wrong "+ answerCounter);
+      //     }
+      //     //Buttons
+      //     $(".nextButton").show();
+      //     $(".submitButton").hide();
+      //   });
+      // }
 
 
 
-      checkAnswer(1, "#trueFalse", "#true");
+      checkAnswer(1, "#itsTrueFalse", "#true");
       checkAnswer(2, "#multipleChoice", "#mcB");
-      checkAnswer(3, "#trueFalse", "#false");
+      checkAnswer(3, "#itsTrueFalse", "#false");
 
     }
     else if (questionCounter === (numOfQuestions+1)){
@@ -87,5 +161,6 @@ $(document).ready(function(){
       $(".questionStart").text("Thank you for playing. You answered "+answerCounter+" out of "+(questionCounter-1)+" questions correctly.");
     }
     questionCounter++;
+    console.log("After Adding "+questionCounter);
   });
 });
