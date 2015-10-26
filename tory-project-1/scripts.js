c7bcd1fa9c36a3b4965f396bda7643d959189073
Tory Burgett <vdburgett@gmail.com>
2015-10-26 10:35:59 -0400

    Monday morning additions

--- tory-project-1/scripts.js
+++ tory-project-1/scripts.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   $(".question").hide();
   $("#question0").show();
   $(".answerArea").hide();
-  $(".refreshArea").hide();
-  $(".submitArea").hide();
+  $(".refreshButton").hide();
+  $(".submitButton").hide();
 
   //Counts what question you are currently on, How many questions did you get right, how many questions have I made?
   var questionCounter = 1;
@@ -23,112 +23,67 @@ $(document).ready(function(){
     }
   });
 
-
+//When on the right question, check answer pulls up the right answerType and verifies that the correct answer has the right background color when submit is clicked
   var checkAnswer = function(questionNumber, answerType, correctAnswer){
     if (questionCounter===questionNumber){
+      $(".answerArea").hide();
       $(answerType).show();
-      var finalAnswer = 0;
-      // $(".answerChoice").on("click", function(){
-      //   if ($(this).css("background")!=selectedColor){
-      //     $(".answerChoice").css("background", unselectedColor);
-      //     $(this).css("background", selectedColor);
-      //   }
-      // });
-      $(correctAnswer).on("click", function(){
-        if(finalAnswer===0){
+      $(".submitButton").show();
+
+      $(".submitButton").on("click", function(){
+        event.preventDefault();
+        if ($(correctAnswer).css("background") == selectedColor){
           answerCounter++;
-          finalAnswer = 1;
-          console.log(answerCounter);
         }
-        else if (finalAnswer===1){
-          answerCounter--;
-          finalAnswer = 0;
-          console.log(answerCounter);
+        else{
+          console.log("wrong answer");
         }
+        // else if (($(correctAnswer).css("background")!=selectedColor)&&()) {
+        //
+        // }
+
+        $(".nextArea").show();
       });
+      // $(".submitArea").hide();
+
     }
   };
-
-
-  // var checkAnswer = function(questionNumber, answerType, correctAnswer){
-  //   if (questionCounter===questionNumber){
-  //     $(answerType).show();
-  //     var finalAnswer = 0;
-  //     $(".answerChoice").on("click", function(){
-  //       if(($(correctAnswer).css("background")===selectedColor) && (finalAnswer===0)){
-  //         answerCounter++;
-  //         finalAnswer = 1;
-  //         console.log(answerCounter);
-  //       }
-  //       else if (($(correctAnswer).css("background") === unselectedColor) && (finalAnswer===1)){
-  //         answerCounter--;
-  //         finalAnswer = 0;
-  //         console.log(answerCounter);
-  //       }
-  //     });
-  //   }
-  // };
-
-  // var checkAnswer = function(questionNumber, answerType, correctAnswer){
-  //   if ((questionCounter===questionNumber)){
-  //     $(answerType).show();
-  //     $(".nextButton").on("click", function(){
-  //       event.preventDefault();
-  //       if($(correctAnswer).css("background")===selectedColor){
-  //         answerCounter++;
-  //       }
-  //     });
-  //   }
-  // };
 
 
   $(".nextButton").on("click", function(){
     event.preventDefault();
     $(".nextButton").text("NEXT");
     if (questionCounter < (numOfQuestions+1)){
-      $(".nextArea").hide();
-      $(".submitArea").show();
+      console.log("questionCounter "+questionCounter);
+
+      $(".nextButton").hide();
       $("#question"+(questionCounter-1)).hide();
       $("#question"+questionCounter).show();
-      $(".answerArea").hide();
 
-      $(".submitButton").on("click", function(){
-        event.preventDefault();
+      //get into what answers to show, and how to grade the answers
+      //temp guide: answer to first=true, second=B, third=false.
+
+    // if 
 
 
-        //get into what answers to show, and how to grade the answers
-        //temp guide: answer to first=true, second=B, third=false.
 
-        checkAnswer(1, "#trueFalse", "#true");
-        checkAnswer(2, "#multipleChoice", "#mcB");
-        checkAnswer(3, "#trueFalse", "#false");
+      checkAnswer(1, "#trueFalse", "#true");
+      checkAnswer(2, "#multipleChoice", "#mcB");
+      checkAnswer(3, "#trueFalse", "#false");
 
-        // if ((questionCounter===1)){
-        //   $("#trueFalse").show();
-        //   $("#true").on("click", function(){
-        //     answerCounter++;
-        //   });
-        // }
-        // else if ((questionCounter===2)) {
-        //   $("#multipleChoice").show();
-        //   $("#mcB").on("click", function(){
-        //     answerCounter++;
-        //   });
-        // }
-        // else if ((questionCounter===3)){
-        //   $("#trueFalse").show();
-        //   $("#false").on("click", function(){
-        //     answerCounter++;
-        //   });
-        // }
-      });
     }
     else if (questionCounter === (numOfQuestions+1)){
-      $("#question"+(questionCounter-1)).hide();
+      //Answers
       $(".answerArea").hide();
-      $(".nextArea").hide();
+
+      //Buttons
+      $(".nextButton").hide();
+      $(".submitButton").hide();
+      $(".refreshButton").show();
+
+      //Questions
+      $("#question"+(questionCounter-1)).hide();
       $(".questionStart").show();
-      $(".refreshArea").show();
       $(".questionStart").text("Thank you for playing. You answered "+answerCounter+" out of "+(questionCounter-1)+" questions correctly.");
     }
     questionCounter++;
