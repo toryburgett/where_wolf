6239046503d014ea854cf602a710765deb283349
Tory Burgett <vdburgett@gmail.com>
2016-01-19 14:32:16 -0500

    progress bar updated

--- tory-project-1/script/scripts.js
+++ tory-project-1/script/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
   var quizNumOn;
   var answeredWrong;
   var answeredRight;
+  var calcProgress;
   var answerSelect = 4;
   var url = "https://gist.githubusercontent.com/toryburgett/71493221c7927a506592/raw/6f2e585b33cc93ffffadbc28888a9e0a5ae9d634/quiz.json";
 
@@ -15,7 +16,15 @@ $(document).ready(function(){
     quizNumOn = 0;
     answeredWrong = 0;
     answeredRight = 0;
+    renderProgress();
   };
+  //progressBar Area
+  var renderProgress = function(){
+    calcProgress = ((quizNumOn)/quizNum)*100;
+    $("#progressDone").css("width", (calcProgress+"%"));
+    $("#progressToGo").css("width", ((100-calcProgress)+"%"));
+  };
+
   // toggle submit button until answer is selected
   var holdSubmit = function(){
     if(answerSelect === 4){
@@ -37,6 +46,7 @@ $(document).ready(function(){
     $(".qText").html("<h1>"+quizQuest.questionText+"</h1>");
     optionsRender(quizQuest.options);
     quizNumOn ++;
+    // renderProgress();
     console.log("quizNumOn " + quizNumOn);
   };
   //render the option, create event listener for option
@@ -59,30 +69,42 @@ $(document).ready(function(){
     }
   };
   //check that the answer matches with correct one
-  var checkAnswer = function(correctAnswer){
-    if(correctAnswer == answerSelect){
+  var checkAnswer = function(question){
+    if(question.correctAnswer == answerSelect){
       answeredRight ++;
+      renderAnswer("Correct! ", "lime", question);
     } else {
       answeredWrong ++;
+      renderAnswer("Wrong, ", "pink", question);
     }
+    $(".answerArea").show();
     showVisible("#next", "#submit", "#wait");
   };
+  var renderAnswer = function(status, color, question){
+    $(".answerExplanation").html(status+" "+question.answerExplained);
+    $(".answerExplanation").css("background-color", color);
+  };
 
+
+  //Render final screen, show renderScore
   var renderScore = function(){
     showVisible("#quizScore", "#welcomeIndex", "#quizGame");
     $(".score").html("total: right = "+answeredRight+" | wrong = "+answeredWrong);
     $("#home").on("click", welcomeIndexShow);
   };
 
+  // render a new question unless you have finished quiz
   var playThisQuiz = function(){
-    if (quizNumOn === quiz.quiz.length){
+    $(".answerArea").hide();
+    if (quizNumOn === quizNum){
       renderScore();
     } else {
       showVisible("#quizGame", "#welcomeIndex", "#quizScore");
       newQuestionRender(quiz.quiz[quizNumOn]);
       $("#submit").off();
       $("#submit").on("click", function(){
-        checkAnswer(quiz.quiz[(quizNumOn-1)].correctAnswer);
+        renderProgress();
+        checkAnswer(quiz.quiz[(quizNumOn-1)]);
       });
     }
   };
@@ -92,7 +114,7 @@ $(document).ready(function(){
   $.ajax({ url: url, type: "get", dataType: "json",})
     .then(function(response){
     quiz = response;
-    quizNum = quiz.length;
+    quizNum = quiz.quiz.length;
     console.log(quiz);
     $("#playQuiz").on("click", playThisQuiz);
     $("#next").on("click", playThisQuiz);
