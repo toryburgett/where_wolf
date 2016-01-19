3804ab8148a05c7c20416df0c15c7de01c830d20
Tory Burgett <vdburgett@gmail.com>
2016-01-18 20:10:12 -0500

    Can shuffle between questions

--- tory-project-1/script/scripts.js
+++ tory-project-1/script/scripts.js
@@ -1,41 +1,51 @@
 $(document).ready(function(){
 
-  //hide quizGame
-  $("#quizGame").hide();
-
-  //ajax request to quiz database
+  //variables
   var quiz;
   var quizNum;
-  var quizNumOn = 0;
+  var quizNumOn;
   var url = "https://gist.githubusercontent.com/toryburgett/71493221c7927a506592/raw/11fdde44e37630f59cf72b4143d399eda2fa8d69/quiz.json";
 
-
-  var playThisQuiz = function(num){
+  // show pages
+  var welcomeIndexShow = function(){
+    $("#welcomeIndex").show();
+    $("#quizGame").hide();
+    quizNumOn = 0;
+  };
+  var quizGameShow = function(){
     $("#welcomeIndex").hide();
     $("#quizGame").show();
-    console.log("hello!");
-    $(".qTitle").html("<h1>"+quiz.quiz[num].questionTitle+"</h1>");
-    $(".qText").html("<h1>"+quiz.quiz[num].questionText+"</h1>");
+  };
+  var newQuestionRender = function(quizQuest){
+    $(".qTitle").html("<h1>"+quizQuest.questionTitle+"</h1>");
+    $(".qText").html("<h1>"+quizQuest.questionText+"</h1>");
     quizNumOn ++;
-    console.log("quizNumOn "+quizNumOn);
-    $("#playQuiz").on("click", function(){
-      playThisQuiz(quizNumOn);
-    });
+    console.log("quizNumOn " + quizNumOn);
   };
 
+
+  var playThisQuiz = function(){
+    if (quizNumOn === quiz.quiz.length){
+      welcomeIndexShow();
+    } else {
+      quizGameShow();
+      newQuestionRender(quiz.quiz[quizNumOn]);
+    }
+  };
+
+
+  // ajax request to the quiz database
+  welcomeIndexShow();
   $.ajax({
     url: url,
     type: "get",
     dataType: "json",
-    }).then(function(response){
-      quiz = response;
-      quizNum = quiz.length;
-
-      console.log(quiz);
-
-      $("#playQuiz").on("click", function(){
-        playThisQuiz(quizNumOn);
-      });
-
+  }).then(function(response){
+    quiz = response;
+    quizNum = quiz.length;
+    console.log(quiz);
+    $("#playQuiz").on("click", playThisQuiz);
+    $("#submit").on("click", playThisQuiz);
   });
+
 });
