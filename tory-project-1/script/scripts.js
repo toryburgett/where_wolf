fec9f3554c94f02ebdd41fd291f56073cc85c301
Tory Burgett <vdburgett@gmail.com>
2016-01-19 09:49:35 -0500

    Changes to script logic, saves answers to questions

--- tory-project-1/script/scripts.js
+++ tory-project-1/script/scripts.js
@@ -4,24 +4,74 @@ $(document).ready(function(){
   var quiz;
   var quizNum;
   var quizNumOn;
-  var url = "https://gist.githubusercontent.com/toryburgett/71493221c7927a506592/raw/11fdde44e37630f59cf72b4143d399eda2fa8d69/quiz.json";
+  var answeredWrong;
+  var answeredRight;
+  var answerSelect = 4;
+  var url = "https://gist.githubusercontent.com/toryburgett/71493221c7927a506592/raw/c866f01ade53151b001e9369eaf002a42591d45b/quiz.json";
 
   // show pages
   var welcomeIndexShow = function(){
     $("#welcomeIndex").show();
     $("#quizGame").hide();
     quizNumOn = 0;
+    answeredWrong = 0;
+    answeredRight = 0;
   };
   var quizGameShow = function(){
     $("#welcomeIndex").hide();
     $("#quizGame").show();
   };
+  // toggle submit button until answer is selected
+  var holdSubmit = function(){
+    if(answerSelect === 4){
+      $("#submit").hide();
+      $("#wait").show();
+      $(".optionTitle").css("background-color", "white");
+    }else{
+      $("#submit").show();
+      $("#wait").hide();
+    }
+  };
+  //render the question
   var newQuestionRender = function(quizQuest){
     $(".qTitle").html("<h1>"+quizQuest.questionTitle+"</h1>");
     $(".qText").html("<h1>"+quizQuest.questionText+"</h1>");
+    optionsRender(quizQuest.options);
     quizNumOn ++;
     console.log("quizNumOn " + quizNumOn);
   };
+  //render the option, create event listener for option
+  var optionClickListen = function(num){
+    $(".option"+num).off();
+    $(".option"+num).on("click", function(){
+      answerSelect = num;
+      holdSubmit();
+      $(".optionTitle").css("background-color", "white");
+      $(".option"+num).css("background-color", "pink");
+      console.log("answerSelect = " + answerSelect);
+    });
+  };
+  var optionsRender = function(options){
+    answerSelect = 4;
+    holdSubmit();
+    for(var i = 0; i < options.length; i++){
+      $(".option"+ i).html("<h1>"+options[i]+"</h1>");
+      optionClickListen(i);
+    }
+  };
+
+  var checkAnswer = function(correctAnswer){
+    if(correctAnswer === answerSelect){
+      answeredRight ++;
+      console.log("right - " + answeredRight);
+    } else {
+      answeredWrong ++;
+      console.log("wrong - " + answeredWrong);
+    }
+
+  };
+
+
 
 
   var playThisQuiz = function(){
@@ -30,22 +80,21 @@ $(document).ready(function(){
     } else {
       quizGameShow();
       newQuestionRender(quiz.quiz[quizNumOn]);
+
+      // checkAnswer(quiz.quiz[quizNumOn].correctAnswer);
+      console.log(quiz.quiz[quizNumOn]);
+      console.log("correctAnswer "+quiz.quiz[quizNumOn].correctAnswer);
     }
   };
 
-
   // ajax request to the quiz database
   welcomeIndexShow();
-  $.ajax({
-    url: url,
-    type: "get",
-    dataType: "json",
-  }).then(function(response){
+  $.ajax({ url: url, type: "get", dataType: "json",})
+    .then(function(response){
     quiz = response;
     quizNum = quiz.length;
     console.log(quiz);
     $("#playQuiz").on("click", playThisQuiz);
     $("#submit").on("click", playThisQuiz);
   });
-
 });
